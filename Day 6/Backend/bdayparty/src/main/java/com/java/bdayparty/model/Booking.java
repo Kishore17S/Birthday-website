e514dc79111488fd27633823b9bacba6ddd2211a
Kishore17S <144918482+Kishore17S@users.noreply.github.com>
2024-03-26 12:16:34 +0530

    Add files via upload

---
+++ Day 6/Backend/bdayparty/src/main/java/com/java/bdayparty/model/Booking.java
@@ -0,0 +1,48 @@
+package com.java.bdayparty.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="_booking")
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+        private String bookingid;
+
+    private String applicantName;
+
+    private String address;
+
+    private String mobileno;
+
+    private Date date;
+
+       @ManyToOne
+    @JoinColumn(name="food_menu_id")
+    private FoodMenu foodmenu;
+
+    @ManyToOne
+    @JoinColumn(name="addon_id")
+    private Addon addon;
+
+    @ManyToOne
+    @JoinColumn(name="themeid")
+    private Theme theme;
+    
+}
